54b3e331776abdfe0660d711bc628f702ba25f1a
mohammadawwad <mohd-awwad@hotmail.com>
2021-03-06 16:02:00 -0800

    publishing code

---
+++ src/Endless.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class Endless : MonoBehaviour
+{
+    public Rigidbody floor;
+    public float forwardforce = 200f;
+    // Update is called once per frame
+    void Update()
+    {
+        floor.AddForce(0, 0, forwardforce * Time.deltaTime);
+    }
+}
---
+++ src/Movement.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardforce = 200f;
+    public float sidewayforce = 100f;
+
+    void FixedUpdate ()
+    {
+        rb.AddForce(0, 0, forwardforce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))       {
+            rb.AddForce(-sidewayforce* Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (rb.position.y < -1)
+
+
+     
+        {
+           
+        }
+    }
+
+
+
+
+
+
+
+}
